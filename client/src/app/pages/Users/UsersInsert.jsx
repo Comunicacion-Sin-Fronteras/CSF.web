71d153299843ccb565c4641d535b11f024024347
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2021-11-22 21:57:46 -0600

    Users page's were added

---
+++ client/src/app/pages/Users/UsersInsert.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class UsersInsert extends Component {
+    render() {
+        return (
+            <div>
+                <p>In this page you'll see the form to add a user</p>
+            </div>
+        );
+    }
+}
+
+export default UsersInsert;
---
+++ client/src/app/pages/Users/index.js
@@ -0,0 +1,5 @@
+import UsersList from './SeniasList'
+import UsersInsert from './SeniasInsert'
+import UsersUpdate from './SeniasUpdate'
+
+export { UsersList, UsersInsert, UsersUpdate }
