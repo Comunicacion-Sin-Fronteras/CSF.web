2a5e24eee1d9acc29b1713ef73364cb0741df82b
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2021-11-28 11:20:34 -0600

    Se agregó implementación de Auth

--- server/database/index.js
+++ server/database/index.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose')
+require("dotenv").config()
+
+const url = process.env.MONGO_DB_CONNECTION_STRING;
 
 mongoose
-    .connect('mongodb://superuser:admin@localhost:27017/ComunicacionSinFronteras?authSource=admin', { useNewUrlParser: true })
+    .connect(url, { useNewUrlParser: true }).then(
+        console.info("mongo connected at:" + url)
+    )
     .catch(e => {
-        console.error('Connection error', e.message)
+        console.error('Connection error:', e.message)
+        // console.error(`mongo: ${url}`)
     })
 
 const db = mongoose.connection
--- server/router/router.js
+++ server/router/router.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const router = express.Router()
 
 const generalController = require('../controllers/senia.controller')
 
-const router = express.Router()
-
+//User
+var userRoutes = require('./user')
+router.get('/', function(){}).use('/user', userRoutes)
 router.post('/senia', generalController.createSenia)
 router.put('/senia/:id', generalController.updateSenia)
 router.delete('/senia/:id', generalController.deleteSenia)
