0bf2111df24e322cd07ab812e90b37c09c313c6e
David Perez Antonio <david.perez.antonio51@gmail.com>
2021-11-30 15:18:15 -0600

    Se agregan mas controladores a Administrador

--- server/router/router.js
+++ server/router/router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const generalController = require('../controllers/senia.controller')
+const AdminController = require('../controllers/administrador.controller')
 
 const router = express.Router()
 
@@ -9,5 +10,10 @@ router.put('/senia/:id', generalController.updateSenia)
 router.delete('/senia/:id', generalController.deleteSenia)
 router.get('/senia/:id', generalController.getSeniaById)
 router.get('/senia', generalController.getSenias)
+
+router.post('/admin',AdminController.createNewAdmin)
+router.put('/admin',AdminController.updateDataAdmin)
+router.delete('/admin',AdminController.deleteAdmin)
+router.get('/admin', AdminController.getDataAdmin)
 
 module.exports = router
