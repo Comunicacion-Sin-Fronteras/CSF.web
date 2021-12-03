069956c5f592df5387611d18af8776970a6f4959
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2021-12-02 18:24:19 -0600

    +LocalStrategie

--- server/auth/strategies/LocalStrategy.js
+++ server/auth/strategies/LocalStrategy.js
@@ -3,10 +3,7 @@ const LocalStrategy = require("passport-local").Strategy
 const user = require("../../models/user")
 
 //Called during login/sign up.
-passport.use(new LocalStrategy({
-    usernameField: 'correo_Electronico',
-    passwordField: 'password'
-}, user.authenticate()))
+passport.use(new LocalStrategy(user.authenticate()))
 
 //called while after logging in / signing up to set user details in req.user
 passport.serializeUser(user.serializeUser())
---
+++ server/router/api_descriptor.js
@@ -0,0 +1,11 @@
+// Solo informativo para ruta "/"
+
+module.exports({
+    status: "success, server running.",
+    api_directions: {
+        api_address: "/", status: "available", description: "api_directions_descriptor",
+        api_address: "/senias", status: "available",
+        api_address: "/", status: "available",
+        api_address: "/", status: "available",
+    }
+})
