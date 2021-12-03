2833dff50967a4ace3ef212d09341121508fa1e6
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2021-12-02 18:26:01 -0600

    Se agregó implementación de Auth

--- client/src/app/router/index.js
+++ client/src/app/router/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState, useContext } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { SeniasList, SeniasInsert, SeniasUpdate } from '../pages/Senias'
@@ -13,68 +13,22 @@ function AppRouting() {
     const [isLoading, setLoading] = useState();
     const [userContext, setUserContext] = useContext(UserContext)
 
-    const verifyUser = useCallback(() => {
-        fetch(process.env.REACT_APP_SERVER_API_ENDPOINT + "user/refreshToken", {
-            method: "POST",
-            credentials: "include",
-            headers: { "Content-Type": "application/json" },
-        }).then(async response => {
-            if (response.ok) {
-                const data = await response.json()
-                setUserContext(oldValues => {
-                    return { ...oldValues, token: data.token }
-                })
-            } else {
-                setUserContext(oldValues => {
-                    return { ...oldValues, token: null }
-                })
-            }
-            // call refreshToken every 5 minutes to renew the authentication token.
-            setTimeout(verifyUser, 5 * 60 * 1000)
-        })
-    }, [setUserContext])
-
-    useEffect(() => {
-        const loggedInUser = window.localStorage.getItem("loggedUser");
-        // const refreshtoken = localStorage.getItem("refreshToken");
-        if (loggedInUser) {
-            const user = JSON.parse(loggedInUser);
-            setToken({ token: "1234" });
-
-            console.log("user stted:")
-            console.log(token)
-            verifyUser()
-        }
-    }, [verifyUser])
-
-
-    if (!token) {
-        return (
-            <div>
-                Credentials needed!
-                <Login setToken={setToken} />
-            </div>
-        )
-    } else if (isLoading) {
-        <Loader />
-    } else {
-        return (
-            <Router>
-                <Routes>
-                    <Route path="/senia/list" exact element={<SeniasList />} />
-                    <Route path="/senia/create" exact element={<SeniasInsert />} />
-                    <Route
-                        path="/senia/update/:id"
-                        exact
-                        element={<SeniasUpdate />}
-                    />
-                    <Route path="/" element={<div>Home</div>} />
-                    <Route path="*" element={<NotFoundError />} />
-                </Routes>
-            </Router>
-        )
-    }
-
+function AppRouting() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/senia/list" exact element={<SeniasList/>} />
+                <Route path="/senia/create" exact element={<SeniasInsert/>} />
+                <Route
+                    path="/senia/update/:id"
+                    exact
+                    element={<SeniasUpdate/>}
+                />
+                <Route path="/" element={<div>Home</div>} />
+                <Route path="*" element={<NotFoundError/>} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default AppRouting
