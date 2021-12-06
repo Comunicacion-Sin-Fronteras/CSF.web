1a6be35173c1803f0ce611b90d5b3d02e29a1a5c
SebastianCD <sebastiancipriano2000@gmail.com>
2021-12-06 04:32:17 -0600

    Pantallas home

---
+++ client/src/HomeRegistrado/Card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Container, Body, Title, Text} from './Card2';
+import './app.css';
+
+export function Card({ classes, children, ...restProps }) {
+  return (
+    <Container className={classNames('card', classes)} {...restProps}>
+      {children}
+    </Container>
+  );
+}
+
+Card.Body = function CardBody({ classes, children, ...restProps }) {
+  return (
+    <Body className={classNames('card__body', classes)} {...restProps}>
+      {children}
+    </Body>
+  );
+};
+
+Card.Title = function CardTitle({ classes, children, ...restProps }) {
+  return (
+    <Title className={classNames('card__title', classes)} {...restProps}>
+      {children}
+    </Title>
+  );
+};
+
+Card.Text = function CardText({ classes, children, ...restProps }) {
+  return (
+    <Text className={classNames('card__text', classes)} {...restProps}>
+      {children}
+    </Text>
+  );
+};
