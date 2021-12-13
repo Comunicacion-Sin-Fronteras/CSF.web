6a64694acb6fcc593a3d8af5718c48440a4715a0
SebastianCD <43630732+SebastianCD@users.noreply.github.com>
2021-12-12 22:25:15 -0600

    .

---
+++ client/src/app/pages/Juegos/Volver/index.js
@@ -0,0 +1,18 @@
+import './app.css';
+import Nav from './Nav';
+import Test from './Test';
+
+function Volver() {
+  return (
+    <html>
+      <body className='bodyJuego'>
+        <div>
+          <Nav />
+          <Test />
+        </div>
+      </body>
+    </html>
+  );
+}
+
+export default Volver;
